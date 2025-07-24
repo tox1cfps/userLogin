05dd81fbbe999358de2f9b54e6c55dae94e6af82
tox1cfps <arthurrodriguesfonseca@gmail.com>
2025-07-23 23:59:50 -0300

    Create func LoginUser and added password hashing

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module userLogin
 go 1.24.4
 
 require github.com/lib/pq v1.10.9
+
+require golang.org/x/crypto v0.40.0
--- internal/ui/cli.go
+++ internal/ui/cli.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"userLogin/config"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Menu() {
@@ -24,7 +26,7 @@ func Menu() {
 		case 1:
 			CreateUser()
 		case 2:
-			// LoginUser()
+			LoginUser()
 		case 3:
 			ListUsers()
 		case 4:
@@ -54,13 +56,46 @@ func CreateUser() {
 	fmt.Print("\nDigite a sua senha: ")
 	fmt.Scan(&password)
 
-	_, err := config.DB.Exec("INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3)", username, email, password)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println("Erro ao gerar senha:", err)
+		return
+	}
+
+	_, err = config.DB.Exec("INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3)", username, email, string(hashedPassword))
 	if err != nil {
 		log.Println("Erro ao criar usuário:", err)
 		return
 	}
 
 	log.Println("Usuário criado com sucesso!")
+}
+
+func LoginUser() {
+	var email, password string
+
+	fmt.Print("\nDigite seu email: ")
+	fmt.Scan(&email)
+
+	fmt.Print("\nDigite sua senha: ")
+	fmt.Scan(&password)
+
+	var username, hashedPassword string
+
+	err := config.DB.QueryRow("SELECT username, password_hash FROM users WHERE email=$1", email).Scan(&username, &hashedPassword)
+	if err != nil {
+		log.Println("Email não encontrado ou erro na consulta", err)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		log.Println("Senha incorreta!")
+		return
+	}
+
+	fmt.Printf("Login bem-sucedido! Bem vindo de volta, %s.\n", username)
+
 }
 
 func ListUsers() {
@@ -111,7 +146,13 @@ func UpdateUser() {
 	fmt.Print("\nDigite a nova senha do usuário: ")
 	fmt.Scan(&novaSenha)
 
-	result, err := config.DB.Exec("UPDATE users SET username=$1, email=$2, password_hash=$3 WHERE id=$4", novoUsername, novoEmail, novaSenha, id)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(novaSenha), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println("Erro ao atualizar a senha!", err)
+		return
+	}
+
+	result, err := config.DB.Exec("UPDATE users SET username=$1, email=$2, password_hash=$3 WHERE id=$4", novoUsername, novoEmail, string(hashedPassword), id)
 	if err != nil {
 		log.Println("Erro ao atualizar usuário:", err)
 		return
